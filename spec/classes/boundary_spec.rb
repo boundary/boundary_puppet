772d438d0a8a805b83e78dbcbec8864a1549755b
Vern Burton <vburton@sportsdigita.com>
2015-07-23 15:18:12 -0500

    adding more test for versions

--- spec/classes/boundary_spec.rb
+++ spec/classes/boundary_spec.rb
@@ -17,7 +17,7 @@ describe 'boundary', :type => 'class' do
 
   end
 
-  context "On Ubuntu OS with no package name specified" do
+  context "On Ubuntu 12.04 with no package name specified" do
     let :facts do
       {
           :osfamily => 'Debian',
@@ -32,12 +32,29 @@ describe 'boundary', :type => 'class' do
 
   end
 
+  context "On Ubuntu 14.04 with no package name specified" do
+    let :facts do
+      {
+          :osfamily => 'Debian',
+          :lsbdistid => 'Debian',
+          :operatingsystem => 'Debian',
+          :lsbdistcodename => 'trusty'
+      }
+    end
+    let(:params) { {:token => 'api-123456',} }
+
+    it 'should compile' do should create_class('boundary') end
+
+  end
+
   context "On RHEL OS with no package name specified" do
     let :facts do
       {
           :osfamily => 'RedHat',
           :lsbdistid => 'RedHat',
           :operatingsystem => 'RedHat',
+          :operatingsystemrelease => 7,
+          :architecture => 'x86_64'
       }
     end
     let(:params) { {:token => 'api-123456',} }
@@ -52,6 +69,8 @@ describe 'boundary', :type => 'class' do
           :osfamily => 'CentOS',
           :lsbdistid => 'CentOS',
           :operatingsystem => 'CentOS',
+          :operatingsystemrelease => 7,
+          :architecture => 'x86_64'
       }
     end
     let(:params) { {:token => 'api-123456',} }
