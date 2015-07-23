b149e490d02133a21359990f4da39cf2452cff63
Vern Burton <vburton@sportsdigita.com>
2015-07-23 10:16:28 -0500

    building test framework for it should compile on Debian, Ubuntu 12.04, RHEL, CentOS

---
+++ spec/classes/boundary_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe 'boundary', :type => 'class' do
+
+  context "On Debian OS with no package name specified" do
+    let :facts do
+      {
+          :osfamily => 'Debian',
+          :lsbdistid => 'Debian',
+          :operatingsystem => 'Debian',
+          :lsbdistcodename => 'jessie'
+      }
+    end
+    let(:params) { {:token => 'api-123456',} }
+
+    it 'should compile' do should create_class('boundary') end
+
+  end
+
+  context "On Ubuntu OS with no package name specified" do
+    let :facts do
+      {
+          :osfamily => 'Debian',
+          :lsbdistid => 'Debian',
+          :operatingsystem => 'Debian',
+          :lsbdistcodename => 'precise'
+      }
+    end
+    let(:params) { {:token => 'api-123456',} }
+
+    it 'should compile' do should create_class('boundary') end
+
+  end
+
+  context "On RHEL OS with no package name specified" do
+    let :facts do
+      {
+          :osfamily => 'RedHat',
+          :lsbdistid => 'RedHat',
+          :operatingsystem => 'RedHat',
+      }
+    end
+    let(:params) { {:token => 'api-123456',} }
+
+    it 'should compile' do should create_class('boundary') end
+
+  end
+
+  context "On CentOS with no package name specified" do
+    let :facts do
+      {
+          :osfamily => 'CentOS',
+          :lsbdistid => 'CentOS',
+          :operatingsystem => 'CentOS',
+      }
+    end
+    let(:params) { {:token => 'api-123456',} }
+
+    it 'should compile' do should create_class('boundary') end
+
+  end
+
+end
